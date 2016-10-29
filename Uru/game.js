16e086ca67f91af6889f1bbcd093272aea99686e
erndidi <erndidi@outlook.com>
2016-10-28 23:57:19 -0500

    First Commit

---
+++ Uru/game.js
@@ -0,0 +1,72 @@
+﻿GameStates.Game = function (game) {
+
+};
+
+var map;
+var backgroundLayer;
+var blockLayer;
+var bg;
+var player;
+var jumpTimer = 0;
+var cursors;
+var jumpButton;
+
+
+GameStates.Game.prototype = {
+
+    create: function () {
+        //below code creates a simple tween animation. You will want to delete this when adding your code
+        //var logo = this.add.sprite(this.world.centerX, this.world.centerY, 'logo');
+        //logo.anchor.setTo(0.5, 0.5);
+        //logo.scale.setTo(0.2, 0.2);
+        //this.add.tween(logo.scale).to({ x: 1, y: 1 }, 2000, Phaser.Easing.Bounce.Out, true);
+
+        map = this.add.tilemap('myTilemap');
+        map.addTilesetImage('tilesetOpenGame', 'myTileset');
+
+       // backgroundLayer = map.createLayer('background');
+        bg = this.add.tileSprite(0, 0, 640, 640, 'bg');
+        blockLayer = map.createLayer('blocklayer');
+        this.physics.arcade.gravity.y = 300;
+        this.setupPlayer();
+        cursors = this.input.keyboard.createCursorKeys();
+        jumpButton = this.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR);       
+        map.setCollision(44, true, 'blocklayer');
+    },
+
+    setupPlayer: function ()
+    {
+        player = this.add.sprite(50, 32, 'player'); //50 x 32 = starting position
+        this.physics.enable(player, Phaser.Physics.ARCADE);
+        player.scale.setTo(1.1, 1.1);
+        player.body.collideWorldBounds = true;
+        player.body.setSize(20, 32, 0, 0);
+        player.anchor.setTo(.5, 1); //so it flips around its middle
+        player.animations.add('move', [5, 6, 7, 8], 10, true);
+    },
+
+    update: function () {
+        player.body.velocity.x = 0; //default speed - stationary
+        game.physics.arcade.collide(player, blockLayer);
+        if (cursors.left.isDown) {
+            player.scale.x = -1;
+            player.body.velocity.x = -150;
+            player.animations.play('move');
+        }
+        else if (cursors.right.isDown) {
+            player.scale.x = 1;
+            player.body.velocity.x = 150;
+            player.animations.play('move');
+        }
+        else {
+            player.animations.stop();
+            player.frame = 5;
+        }
+        if (jumpButton.isDown && player.body.onFloor() && this.time.now > jumpTimer) {
+            player.body.velocity.y = -250;
+            jumpTimer = game.time.now + 750;
+        }
+    },
+   
+    render: function () { },
+};
---
+++ Uru/preloader.js
@@ -0,0 +1,24 @@
+﻿// Preloader will load all of the assets like graphics and audio
+GameStates.Preloader = function (game) {
+    this.preloadBar = null;
+}
+
+GameStates.Preloader.prototype = {
+    preload: function () {
+        // common to add a loading bar sprite here...
+        //this.preloadBar = this.add.sprite(this.game.width / 2 - 100, this.game.height / 2, 'preloaderBar');
+        //this.load.setPreloadSprite(this.preloadBar);
+        // load all game assets
+        // images, spritesheets, atlases, audio etc..
+        this.load.image('logo', 'assets/phaser2.png');
+        this.load.tilemap('myTilemap', 'assets/tilemaps/uruMap.json', null, Phaser.Tilemap.TILED_JSON);
+        this.load.image('myTileset', "assets/tilemaps/tilesetOpenGame.png");
+        this.load.image('bg', 'assets/tiledbackground.png');
+        this.load.spritesheet('player', 'assets/spritesheets/Unicorn_Run48.png', 60, 48)
+    },
+
+    create: function () {  
+        //call next state
+        this.state.start('Game');
+    }
+};
